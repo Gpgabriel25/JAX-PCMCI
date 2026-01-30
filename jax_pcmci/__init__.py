99219e0a6ece86f2391397858f2fe8c312cdceb6
Gabriel Garcia <66880445+Gpgabriel25@users.noreply.github.com>
2026-01-29 21:35:54 -0500

    updated to v1.3.0

--- jax_pcmci/__init__.py
+++ jax_pcmci/__init__.py
@@ -39,7 +39,7 @@ For more examples and detailed documentation, visit:
 https://jax-pcmci.readthedocs.io
 """
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 __author__ = "JAX-PCMCI Contributors"
 
 # Core imports
